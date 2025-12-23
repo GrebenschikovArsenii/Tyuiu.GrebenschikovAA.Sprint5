dd2c807d2656256302747cb8f4d6c36f02a4acae
BaimuhametovaMadina <fryecest@gmail.com>
2025-12-24 03:00:54 +0500

    task0

--- Tyuiu.GrebenschikovAA.Sprint5.Task0.V19.Test/DataServiceTest.cs
+++ Tyuiu.GrebenschikovAA.Sprint5.Task0.V19.Test/DataServiceTest.cs
@@ -1,11 +1,24 @@
+using Tyuiu.GrebenschikovAA.Sprint5.Task0.V19.Lib;
+
 namespace Tyuiu.GrebenschikovAA.Sprint5.Task0.V19.Test
 {
     [TestClass]
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCalculationAndFileSave()
         {
+            DataService ds = new DataService();
+            int x = 3;
+
+            string path = ds.SaveToFileTextData(x);
+            Assert.IsTrue(File.Exists(path));
+
+            string result = File.ReadAllText(path);
+            double value = Convert.ToDouble(result);
+
+            double expected = Math.Round((2 * Math.Pow(x, 2) - 1) / Math.Sqrt(Math.Pow(x, 2) - 2), 3);
+            Assert.AreEqual(expected, value);
         }
     }
 }
--- Tyuiu.GrebenschikovAA.Sprint5.Task0.V19/Program.cs
+++ Tyuiu.GrebenschikovAA.Sprint5.Task0.V19/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Tyuiu.GrebenschikovAA.Sprint5.Task0.V19
+﻿using Tyuiu.GrebenschikovAA.Sprint5.Task0.V19.Lib;
+
+namespace Tyuiu.GrebenschikovAA.Sprint5.Task0.V19
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Гребенщиков А.А. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Гребенщиков Арсений | ИБКСб-24-1                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение, вычислить его значение при x = 2,                       *");
+            Console.WriteLine("* результат сохранить в текстовый файл OutPutFileTask0.txt                *");
+            Console.WriteLine("* и вывести на консоль. Округлить до трёх знаков после запятой.           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 2.12x^3 + 1.05x^2 + 4.1x * 2, x=2                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int x = 3;
+            string path = ds.SaveToFileTextData(x);
+
+            Console.WriteLine($"Результат вычислений сохранен в файл: {path}");
+            Console.WriteLine($"Содержимое файла: {File.ReadAllText(path)}");
+            Console.ReadLine();
         }
     }
 }
