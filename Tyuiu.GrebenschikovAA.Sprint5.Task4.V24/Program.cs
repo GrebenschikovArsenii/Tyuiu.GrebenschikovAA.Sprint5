4a07a6d38352ba5f00ff8f2ed7f71f65c754bd8c
BaimuhametovaMadina <fryecest@gmail.com>
2025-12-24 03:20:05 +0500

    task4

--- Tyuiu.GrebenschikovAA.Sprint5.Task4.V24/Program.cs
+++ Tyuiu.GrebenschikovAA.Sprint5.Task4.V24/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Tyuiu.GrebenschikovAA.Sprint5.Task4.V24
+﻿using Tyuiu.GrebenschikovAA.Sprint5.Task4.V24.Lib;
+
+namespace Tyuiu.GrebenschikovAA.Sprint5.Task4.V24
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Гребенщиков А.А. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Гребенщиков Арсений | ИБКСб-24-1                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение, вычислить его значение при x = 2,                       *");
+            Console.WriteLine("* результат сохранить в текстовый файл OutPutFileTask0.txt                *");
+            Console.WriteLine("* и вывести на консоль. Округлить до трёх знаков после запятой.           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 2.12x^3 + 1.05x^2 + 4.1x * 2, x=2                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = Path.Combine("C:", "DataSprint5", "InPutDataFileTask4V29.txt");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadLine();
         }
     }
 }
