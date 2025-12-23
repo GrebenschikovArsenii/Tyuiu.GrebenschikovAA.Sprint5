9901d673c84d3c34d98b10a972c6f331f9df352a
BaimuhametovaMadina <fryecest@gmail.com>
2025-12-24 02:52:28 +0500

    task7

--- Tyuiu.GrebenschikovAA.Sprint5.Task7.V27.Test/DataServiceTest.cs
+++ Tyuiu.GrebenschikovAA.Sprint5.Task7.V27.Test/DataServiceTest.cs
@@ -1,11 +1,48 @@
+using Tyuiu.GrebenschikovAA.Sprint5.Task7.V27.Lib;
+
 namespace Tyuiu.GrebenschikovAA.Sprint5.Task7.V27.Test
 {
     [TestClass]
     public class DataServiceTest
     {
+        DataService ds = new DataService();
+
+        string path = Path.Combine(Path.GetTempPath(), "InPutDataFileTask7V27.txt");
+        //string newPath = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V27.txt");
+
         [TestMethod]
-        public void TestMethod1()
+        public void CheckPath()
         {
+            FileInfo fileInfo = new FileInfo(path);
+            Assert.IsTrue(fileInfo.Exists);
+
+        }
+        [TestMethod]
+        public void CheckNewPath()
+        {
+            string newPath = ds.LoadDataAndSave(path);
+            FileInfo fileInfo = new FileInfo(newPath);
+            Assert.IsTrue(fileInfo.Exists);
+
+        }
+
+        [TestMethod]
+        public void CheckValid()
+        {
+            string newPath = ds.LoadDataAndSave(path);
+            string text = "";
+            using (StreamReader reader = new StreamReader(newPath))
+            {
+                text = reader.ReadToEnd();
+
+            }
+
+
+
+
+            string wait = "???????????????????";
+
+            Assert.AreEqual(wait, text);
         }
     }
 }
