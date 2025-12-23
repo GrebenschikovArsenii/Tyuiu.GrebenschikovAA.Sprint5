35ac3bd1b097d94542e8b5a0f73e843b12bcdaf2
BaimuhametovaMadina <fryecest@gmail.com>
2025-12-24 03:16:24 +0500

    task3

--- Tyuiu.GrebenschikovAA.Sprint5.Task3.V20.Lib/DataService.cs
+++ Tyuiu.GrebenschikovAA.Sprint5.Task3.V20.Lib/DataService.cs
@@ -1,5 +1,4 @@
-﻿using System.Text;
-using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.GrebenschikovAA.Sprint5.Task3.V20.Lib
 {
@@ -7,10 +6,7 @@ namespace Tyuiu.GrebenschikovAA.Sprint5.Task3.V20.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            double y = x / (Math.Sqrt(x * x) + x);
-
-
-            y = Math.Round(y, 3);
+            double y = Math.Round(x / (Math.Sqrt(x * x) + x), 3);
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
             BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate));
             writer.Write(y);
