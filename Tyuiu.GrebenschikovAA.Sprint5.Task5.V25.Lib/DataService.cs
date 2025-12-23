b59fcb20706515876c2f0d0845b70d0a7ef74307
BaimuhametovaMadina <fryecest@gmail.com>
2025-12-24 03:22:16 +0500

    task5

--- Tyuiu.GrebenschikovAA.Sprint5.Task5.V25.Lib/DataService.cs
+++ Tyuiu.GrebenschikovAA.Sprint5.Task5.V25.Lib/DataService.cs
@@ -1,7 +1,21 @@
-﻿namespace Tyuiu.GrebenschikovAA.Sprint5.Task5.V25.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
 
+namespace Tyuiu.GrebenschikovAA.Sprint5.Task5.V25.Lib
+{
+    public class DataService: ISprint5Task5V25
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = -5973964.557;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) == null)
+                {
+                    res *= Convert.ToDouble(line);
+                }
+            }
+            return res;
+        }
     }
 }
